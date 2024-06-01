9665d52847c2e12501cd3f433b40e914f42a79f4
tainamsz <tainamsz2410@gmail.com>
2024-05-31 23:42:57 -0300

    Desafio concluído com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("(48)552456789","Iphone 15","23443759603945",128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
+Console.WriteLine("");
+
+Nokia nokia =new Nokia("(21)57854204","Nokia 2002","2334567830495823",256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snapchat");
+nokia.ReceberLigacao();
